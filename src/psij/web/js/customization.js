78e0e51f35c2f73676af4f1bc191243316e293d1
Pascal Aschwanden <aschwanden1@llnl.gov>
2022-08-03 16:36:49 -0700

    Add configurations for curated sight and intro blurbs

--- src/psij/web/js/customization.js
+++ src/psij/web/js/customization.js
@@ -1,3 +1,39 @@
 CUSTOMIZATION = {
     projectName: "PSI/J"
 }
+
+var CONF = {
+    STR: {
+        "title": "PSI/J Dashboard",
+        "sub_title": "Exascale dashboard testing service",
+        "intro_blurb": "The dashboard testing service shows you the status of various sites by date or by lab.",
+        "outro_blurb": "Dashboard outro.  The outro will also be roughly one paragraph long."
+    },
+    "curated_sights": {
+        xaxis: [
+            "PSI/J",
+            "PSI/J + Parsl",
+            "PSI/J + Swift",
+            "RADICAL-Pilot"
+        ],
+        yaxis: [
+            {
+                logo_src: "",
+                logo_name: ""
+            },
+            {
+                logo_src: "",
+                logo_name: ""
+            },
+            {
+                logo_src: "",
+                logo_name: ""
+            },
+            {
+                logo_src: "",
+                logo_name: ""
+            }
+        ]
+    }
+};
+
