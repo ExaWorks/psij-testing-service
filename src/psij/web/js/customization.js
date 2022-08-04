53c7db382084a7f5c5c8da772645397ece31c4e0
Pascal Aschwanden <aschwanden1@llnl.gov>
2022-08-04 08:38:56 -0700

    added new home tab for new curate sites page

--- src/psij/web/js/customization.js
+++ src/psij/web/js/customization.js
@@ -4,12 +4,12 @@ CUSTOMIZATION = {
 
 var CONF = {
     STR: {
-        "title": "PSI/J Dashboard",
+        "title": CUSTOMIZATION.projectName,
         "sub_title": "Exascale dashboard testing service",
         "intro_blurb": "The dashboard testing service shows you the status of various sites by date or by lab.",
         "outro_blurb": "Dashboard outro.  The outro will also be roughly one paragraph long."
     },
-    "curated_sights": {
+    "curated_sites": {
         xaxis: [
             "PSI/J",
             "PSI/J + Parsl",
