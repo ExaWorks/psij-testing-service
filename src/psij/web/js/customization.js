c76829be1a7d28f2147a0ac5fe2b23d250f091c3
Pascal Aschwanden <aschwanden1@llnl.gov>
2022-08-05 10:07:54 -0700

    Update intro text to give example of intro blurbs we could use

--- src/psij/web/js/customization.js
+++ src/psij/web/js/customization.js
@@ -5,8 +5,8 @@ CUSTOMIZATION = {
 var CONF = {
     STR: {
         "dashboardTitle": "Exascale dashboard testing service",
-        "introBlurb": "The dashboard testing service shows you the status of various sites by date or by lab.",
-        "bottomBlurb": "Dashboard outro.  The outro will also be roughly one paragraph long."
+        "introBlurb": "The dashboard testing service shows you the status of various sites in the calendar view by date and machine without curation.",
+        "bottomBlurb": "The curated sites tab will eventually show labs on the left which are expandable to show the machines of each lab on the y axis.  Meanwhile, the X axis will show you test cases like PSI/J and PSI/J Parcel, etc."
     },
     "curated_sites": {
         xaxis: [
@@ -18,11 +18,18 @@ var CONF = {
         yaxis: [
             {
                 logo_src: "",
-                logo_name: ""
+                logo_name: "",
+                machines: [
+                    "oregon1",
+                    "oregon2"
+                ]
             },
             {
                 logo_src: "",
-                logo_name: ""
+                logo_name: "",
+                machines: [
+                    "machine82834"
+                ]
             },
             {
                 logo_src: "",
