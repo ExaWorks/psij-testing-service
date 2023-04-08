2fa74e64c05f8579c0565414bb9ee74a518d7f6f
Pascal Aschwanden <aschwanden1@llnl.gov>
2023-04-07 21:15:17 -0700

    Expand stub and put coverage on a per file basis.  implement coverage use on a per file basis

--- src/psij/web/js/CodeCoverageView.js
+++ src/psij/web/js/CodeCoverageView.js
@@ -3,39 +3,73 @@ var PS = PS || {};
 PS.CodeCoverageView = function() {
 
     var STUB_FROM_BE = {
-        files: [{name: "__init__.py"}, {name: "descriptor.py"}, {name: "job.py"}],
-        coverage: [{
-            lines: [
-                "# this is a sample code file",
-                "# using comments that appear white",
-                ""
-            ],
-            status: "white"
-        },
-        {
-            lines: [
-                "var cx=0;",
-                "cx = 80;",
-                "if( cx == 80 ) { return true; }"
-            ],
-            status: "green"
-        },
+        files: [
             {
-            lines: [
-                "var not_covered = 2;",
-                "if( not_covered ) {",
-                "  return 0;",
-                "}"
-            ],
-            status: "red"
-        }
+                name: "__init__.py",
+                coverage: [{
+                    lines: [
+                        "# this is a sample code file",
+                        "# using comments that appear white",
+                        ""
+                    ],
+                    status: "white"
+                },
+                    {
+                        lines: [
+                            "var cx=0;",
+                            "cx = 80;",
+                            "if( cx == 80 ) { return true; }"
+                        ],
+                        status: "green"
+                    },
+                    {
+                        lines: [
+                            "var not_covered = 2;",
+                            "if( not_covered ) {",
+                            "  return 0;",
+                            "}"
+                        ],
+                        status: "red"
+                    }
+                ]
+            },
+            {
+                name: "descriptor.py",
+                coverage: [{
+                    lines: [
+                        "# this is a sample code file",
+                        "# using comments that appear white",
+                        ""
+                    ],
+                    status: "white"
+                },
+                    {
+                        lines: [
+                            "var cx=0;",
+                            "cx = 80;",
+                            "if( cx == 80 ) { return true; }"
+                        ],
+                        status: "green"
+                    },
+                    {
+                        lines: [
+                            "var not_covered = 2;",
+                            "if( not_covered ) {",
+                            "  return 0;",
+                            "}"
+                        ],
+                        status: "red"
+                    }
+                ]},
+            {name: "job.py",
+                coverage: []}
         ]
     };
 
 
     var make_right_side_ = function() {
 
-        var coverage = STUB_FROM_BE.coverage;
+        var coverage = STUB_FROM_BE.files[0].coverage;
         var ht = "";
 
         for( var x=0; x < coverage.length; x++ ) {
@@ -61,6 +95,7 @@ PS.CodeCoverageView = function() {
 
         $('.select-page').append( ht );
     };
+
 
     var init_ = function() {
 
