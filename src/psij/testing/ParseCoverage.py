821970dee0050a781bbf29bcdb5981d2d2b61b81
Pascal Aschwanden <aschwanden1@llnl.gov>
2023-04-19 09:57:45 -0700

    parse coverage class impl

--- src/psij/testing/ParseCoverage.py
+++ src/psij/testing/ParseCoverage.py
@@ -13,6 +13,19 @@ class ParseCoverage:
         # get root element
         root = tree.getroot()
 
-        # create empty list for news items
-        newsitems = []
+        # iterate over all "class" elements
+        for class_elem in root.iter('class'):
+            # get the name and filename attributes
+            class_name = class_elem.attrib['name']
+            class_filename = class_elem.attrib['filename']
+
+            # iterate over all "line" elements within the "class" element
+            for line_elem in class_elem.iter('line'):
+                # get the number and hits attributes
+                line_number = line_elem.attrib['number']
+                line_hits = line_elem.attrib['hits']
+
+                # do something with the class name, filename, line number, and hits
+                print(f"Class: {class_name}, Filename: {class_filename}, Line: {line_number}, Hits: {line_hits}")
+
         return ["hello", "cov"]
