01a7d7c2aee0b5a3f6f0c6b225073d5aa1c4c2b8
hategan <mhategan@gmail.com>
2023-08-31 10:02:25 -0700

    Fixed parameter.

--- src/psij/testing/service.py
+++ src/psij/testing/service.py
@@ -259,7 +259,7 @@ class TestingAggregatorApp(object):
         maintainer_email = config['maintainer_email']
         site = Site.objects(site_id=site_id).first()
         if site is None:
-            self._update(Site(site_id=id, key='', ip=cherrypy.request.remote.ip))
+            self._update(Site(site_id=site_id, key='', ip=cherrypy.request.remote.ip))
         if maintainer_email:
             site.crt_maintainer_email = maintainer_email
             site.save()
